3cfddb0b38463e0d83ca6d930630db3e042c0315
Md Rijwan Razzaq Matin <mrrmmonty@gmail.com>
2024-02-25 02:51:07 +0800

    build(amplify): adding amplify configs

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "userOnboarding": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
